549a00b3de589a203e5e76e27485bc807c046692
michal100032 <michal100032@gmail.com>
2023-11-17 20:53:18 +0100

    clean up tests/main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,8 +16,8 @@ static const double RAD_TO_DEG = 180.0 / PI;
 
 static const std::string TLE = R""""(
 ISS (ZARYA)             
-1 25544U 98067A   23310.23018765  .00031907  00000+0  56109-3 0  9994
-2 25544  51.6419 344.9652 0001009  76.3839 283.7262 15.50187395423820
+1 25544U 98067A   23321.41134024  .00012713  00000+0  23531-3 0  9990
+2 25544  51.6431 289.5970 0000839 305.0931 191.9975 15.49391010425558
 )"""";
 
 void split_tle(const std::string& tle, char* line_one, char* line_two) {
@@ -87,78 +87,39 @@ int main() {
 	SGP4Funcs::twoline2rv(tle_line_one, tle_line_two,
 		'c', 0, 'i', SGP4Funcs::wgs84, startmfe, stopmfe, deltamin, sgp4_rec);
 	
-	SGP4Funcs_mod::elsetrec sgp4_mod_rec = 
-		SGP4Funcs_mod::twoline2rv(entry);
-
-	SGP4Funcs_mod_near::propagation_coeffs sgp4_near_rec = SGP4Funcs_mod_near::sgp4init(entry);
-
 	sgp4::propagator iss_propagator(TLE);
 
 	while (true) {
-		double pos_sgp4[3], pos_sgp4_mod[3];
-		double vel_sgp4[3], vel_sgp4_mod[3];
-
 		auto now = std::chrono::utc_clock::now();
 
 		double seconds_offset
 			= std::chrono::duration<double>(now - entry.epoch).count();
 
+		auto [pos, vel] = iss_propagator.run(seconds_offset / 60.0);
+
+		sgp4::earth_coords coords =
+			sgp4::from_eci_to_coords_ellipsoid(pos, now);
+
+		std::cout << "Latitude longitude: " << std::endl;
+		std::cout << (coords.latitude * RAD_TO_DEG) << " "
+			<< (coords.longitude * RAD_TO_DEG) << std::endl;
+
+		
+
+		double pos_sgp4[3];
+		double vel_sgp4[3];
+
 		SGP4Funcs::sgp4(sgp4_rec, seconds_offset / 60.0, pos_sgp4, vel_sgp4);
 		if (sgp4_rec.error != 0) {
 			std::cerr << "SGP4 error " << sgp4_rec.error << std::endl;
 		}
-		std::cout << "SGP4: " << std::endl;
-		print_coords_from_pos(pos_sgp4, now);
 
-		SGP4Funcs_mod::sgp4(sgp4_mod_rec, seconds_offset / 60.0, pos_sgp4_mod, vel_sgp4_mod);
-		if (sgp4_mod_rec.error != 0) {
-			std::cerr << "SGP4_mod error " << sgp4_mod_rec.error << std::endl;
+		if (!assert_d3v3(pos_sgp4, pos, "Positions don't match!")) {
+			std::cout << "SGP4: " << std::endl;
+			print_coords_from_pos(pos_sgp4, now);
 		}
 
-		const char* assert_msg =
-			"SGP4s yield different positions";
-		if (!assert_d3(pos_sgp4, pos_sgp4_mod, assert_msg)) {
-			std::cout << "SGP4_mod: " << std::endl;
-			print_coords_from_pos(pos_sgp4_mod, now);
-		}
-
-		auto [pos_mod_near, vel_mod_near] = 
-			SGP4Funcs_mod_near::sgp4(sgp4_near_rec, seconds_offset / 60.0);
-		/*
-		if (sgp4_near_rec.error != 0) {
-			std::cerr << "SGP4_mod_near error " << sgp4_near_rec.error << std::endl;
-		}*/
-
-		if (!assert_d3v3(pos_sgp4, pos_mod_near, assert_msg)) {
-			std::cout << "SGP4_near: " << std::endl;
-			sgp4::earth_coords coords = sgp4::from_eci_to_coords_ellipsoid(pos_mod_near, now);
-
-			std::cout << "Latitude longitude: " << std::endl;
-			std::cout << (coords.latitude * RAD_TO_DEG) << " "
-				<< (coords.longitude * RAD_TO_DEG) << std::endl;
-		}
-
-		const char* assert_msg_2 =
-			"SGP4s yield different velocities";
-
-
-		if (!assert_d3v3(vel_sgp4_mod, vel_mod_near, assert_msg_2)) {
-			std::cout << "SGP4_near: " << std::endl;
-
-			std::cout << vel_sgp4_mod[0] << " " << vel_sgp4_mod[1] << " " << vel_sgp4_mod[2] << std::endl;
-			std::cout << vel_mod_near.x << " " << vel_mod_near.y << " " << vel_mod_near.z << std::endl;
-		}
-
-		if (!assert_d3(vel_sgp4_mod, vel_sgp4, assert_msg_2)) {
-			std::cout << "SPG_mod: " << std::endl;
-
-			std::cout << vel_sgp4_mod[0] << " " << vel_sgp4_mod[1] << " " << vel_sgp4_mod[2] << std::endl;
-			std::cout << vel_sgp4[0] << " " << vel_sgp4[1] << " " << vel_sgp4[2] << std::endl;
-		}
-		auto [pos, vel] = iss_propagator.run(seconds_offset / 60.0);
-		if (vel.x == vel_mod_near.x && vel.y == vel_mod_near.y && vel.z == vel_mod_near.z) {
-			std::cout << "OK" << std::endl;
-		}
+		assert_d3v3(vel_sgp4, vel, "Velocities don't match!");
 
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 		std::cout << std::endl << std::endl;
